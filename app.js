6a72d764684137a0230451b8cb3159a8ba000f0f
yamori-ya <yamorikahou@gmail.com>
2021-02-18 20:16:38 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,116 @@
+
+function getJson(filename) {
+	return new Promise((resolve, reject) => {
+		$.getJSON(filename, data => resolve(data))
+	})
+}
+function createMenuImgUrl(json) {
+	let keys = Object.keys(json)
+	let style = ''
+	keys.forEach((key, i) => {
+		let num = ('00' + (i+1)).slice(-2)
+		style += `.drawer-menu-item.img[num="${num}"]::before { background-image: url(./sprite/${key}); }\r\n`
+	})
+	$(document.head).append(`<style id="menu-url">${style}</style>`)
+}
+function createMenuDom(json) {
+	let keys = Object.keys(json)
+	let dom = ''
+	keys.forEach((key, i) => {
+		let num = ('00' + (i+1)).slice(-2)
+		let name = key.match(/(.+)\./)[1]
+		dom += `<li><a class="drawer-menu-item img" num="${num}">${name}</a></li>`
+	})
+	$('#add-obj').append(dom)
+}
+
+
+// メイン処理
+(async function() {
+	
+	// json取得
+	let json = await getJson('contours.json');
+	let keys = Object.keys(json)
+	
+	// メニューの画像用スタイル作成
+	createMenuImgUrl(json);
+	// メニューdom作成
+	createMenuDom(json);
+	// メニュー生成
+	$(document).ready(() => $('.drawer').drawer());
+	
+	// 物理エンジン開始
+	let engine = new MatterEngine();
+	
+	
+	let addNum = function(num) {
+		let name = keys[num]
+		console.log(num);
+		engine.addVoiceroid(name, json[name])
+	}
+	
+	$('#rm-all').on('click', e =>{
+		engine.clearAll();
+	})
+	$('#rm-rnd').on('click', e => {
+		engine.clearRnd();
+	})
+	$('#rm-lastadd').on('click', e => {
+		engine.clear(engine.lastAdded);
+	})
+	$('#rm-lasttoutch').on('click', e => {
+		engine.clear(engine.lastTouches);
+	})
+	$('#ad-all').on('click', e => {
+		let array = [...keys]
+		for(var i = array.length - 1; i > 0; i--){
+			var r = Math.floor(Math.random() * (i + 1));
+			var tmp = array[i];
+			array[i] = array[r];
+			array[r] = tmp;
+		}
+		for (let key of array) {
+			engine.addVoiceroid(key, json[key])
+		}
+	})
+	$('#ad-rnd').on('click', e => {
+		let num = Math.floor(Math.random() * keys.length);
+		addNum(num)
+	})
+	$('.drawer-menu-item.img').on('click', e => {
+		let num = Number($(e.target).attr('num'))
+		addNum(num-1)
+	})
+	
+	// pow
+	$('#pow').on('click', e => {
+		engine.explosion()
+	})
+	
+	
+	const DEBUG = false;
+	if (DEBUG) {
+		$('#prop').show();
+		$('#frame').on('click', e => engine.toggleWireframes())
+		$('.Offset').on('change', e => {
+			engine.lastAdded.render.sprite[e.target.id + 'Offset'] = e.target.value / 100
+		})
+		$('#num').on('change', e =>  {
+			addNum(e.target.value)
+			let obj = engine.lastAdded
+			$('#x.Offset').val(Math.floor(obj.render.sprite.xOffset * 100))
+			$('#y.Offset').val(Math.floor(obj.render.sprite.yOffset * 100))
+		})
+		$('#copy').on('click', e => {
+			// オフセット情報をクリップボードにコピー
+			let x = $('#x.Offset').val();
+			let y = $('#y.Offset').val();
+			e.target.value = `"xOffset":${x},"yOffset":${y},`;
+			e.target.select();
+			document.execCommand("copy");
+		})
+	}
+	
+})();
+
+
---
+++ engine.js
@@ -0,0 +1,134 @@
+class MatterEngine {
+	constructor() {
+		var engine = this.engine = Matter.Engine.create(document.body, {
+			render: {
+				options: {
+					width: 1000,
+					height: 600,
+					pixelRatio: 1,
+					background: '#fafafa',
+					wireframeBackground: '#222',
+					hasBounds: true,
+					enabled: true,
+					wireframes: false,
+					showSleeping: false,
+					showDebug: false,
+					showBroadphase: false,
+					showBounds: false,
+					showVelocity: false,
+					showCollisions: false,
+					showSeparations: false,
+					showAxes: false,
+					showPositions: false,
+					showAngleIndicator: false,
+					showIds: false,
+					showShadows: false,
+					showVertexNumbers: false,
+					showConvexHulls: false,
+					showInternalEdges: false
+				}
+			}
+		});
+		
+		this._lastAdded = {};
+		this._lastTouches = {};
+		
+		// ゆかり
+		let walls = [
+			Matter.Bodies.rectangle(0,    300, 50, 600, { isStatic: true }), // L
+			Matter.Bodies.rectangle(1000, 300, 50, 600, { isStatic: true }), // R
+			Matter.Bodies.rectangle(500,   0, 1000, 50, { isStatic: true }), // U
+			Matter.Bodies.rectangle(500, 600, 1000, 50, { isStatic: true }), // D
+		] 
+		// クリック機能
+		var mousedrag = Matter.MouseConstraint.create(engine, {
+			element: document.body, 
+			constraint: {
+				render: {
+					strokeStyle: "rgba(255, 255, 255, 255)" //マウスの動きを表示する(白)
+				}
+			}
+		});
+		// updateを上書き
+		let $this = this;
+		let _update = Matter.MouseConstraint.update
+		Matter.MouseConstraint.update = function(mouseConstraint, bodies) {
+			_update(mouseConstraint, bodies)
+			if (mouseConstraint.body) {
+				$this._lastTouches = mouseConstraint.body;
+			}
+		}
+		
+		Matter.World.add(engine.world, [ mousedrag, ...walls ]);
+		
+		// 物理シミュレーションを実行
+		Matter.Engine.run(engine);
+	}
+	
+	get lastAdded() {
+		return this._lastAdded;
+	}
+	get lastTouches() {
+		return this._lastTouches;
+	}
+	toggleWireframes() {
+		this.engine.render.options.wireframes ^= true
+	}
+	
+	addVoiceroid(name, obj) {
+		let points = obj.points.map(i => {return {x:i.x, y:i.y}})
+		let vertices = Matter.Vertices.create(points);
+		
+		let scale = 1;
+		vertices = Matter.Vertices.scale(vertices, scale, scale)
+		
+		let body = Matter.Bodies.fromVertices(500, 200, vertices, {
+			isStatic: false,
+			render: {
+				sprite: {
+					texture: 'sprite/' + name,
+					xScale: scale,
+					yScale: scale
+				}
+			},
+			timeScale: 1
+		}, true);
+		
+		Matter.World.add(this.engine.world, body)
+		if (obj.xOffset != 0) body.render.sprite.xOffset = (obj.xOffset / 100)
+		if (obj.yOffset != 0) body.render.sprite.yOffset = (obj.yOffset / 100)
+		this._lastAdded = body;
+	}
+	
+	clear(body) {
+		Matter.Composite.removeBody(this.engine.world, body);
+	}
+	clearRnd() {
+		let dynamic = this.engine.world.bodies.filter(b => !b.isStatic);
+		let r = Math.floor(Math.random() * (dynamic.length + 1));
+		this.clear(dynamic[r]);
+	}
+	clearAll() {
+		for (let i = this.engine.world.bodies.length; i > 0; i--) {
+			let body = this.engine.world.bodies[i];
+			if (body && !body.isStatic) {
+				this.clear(body);
+			}
+		}
+	}
+	explosion() {
+		var bodies = Matter.Composite.allBodies(this.engine.world);
+		
+		for (var i = 0; i < bodies.length; i++) {
+			var body = bodies[i];
+			if (!body.isStatic /*&& body.position.y >= 500*/) {
+				var forceMagnitude = 0.05 * body.mass;
+				Matter.Body.applyForce(body, body.position, {
+					x: (forceMagnitude + Matter.Common.random() * forceMagnitude) * Matter.Common.choose([1, -1]), 
+					y: -forceMagnitude + Matter.Common.random() * -forceMagnitude
+				});
+			}
+		}
+	};
+	
+}
